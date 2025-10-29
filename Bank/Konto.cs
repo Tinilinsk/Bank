2b4021c09927874596b14182ac7028b6f722bf25
Tinilinsk <120578910+Tinilinsk@users.noreply.github.com>
2025-10-29 23:01:34 +0100

    Добавьте файлы проекта.

---
+++ Bank/Konto.cs
@@ -0,0 +1,38 @@
+﻿namespace Bank
+{
+    public class Konto
+    {
+        private string _id { get; set; }
+        private string _name { get; set; }
+        private int _balance { get; set; }
+
+        public Konto(string id, string name, int balance)
+        {
+            _id = id;
+            _name = name;
+            _balance = balance;
+        }
+        public string getName()
+        {
+            return _name;
+        }
+        public void setName(string name)
+        {
+            _name = name;
+        }
+        public int getBalance()
+        {
+            return _balance;
+        }
+        public void setBalance(int balance)
+        {
+            _balance = balance;
+        }
+        public string getId()
+        {
+            return _id;
+        }
+
+
+    }
+}
---
+++ Bank/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Bank
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Konto Person1 = new Konto("12344321", "Artsiom", 100000);
+            Konto Person2 = new Konto("12387983", "Igor", 1000);
+
+            List<Konto> Users = new List<Konto>();
+            Users.Add(Person1);
+            Users.Add(Person2);
+            foreach (Konto user in Users)
+            {
+                Console.WriteLine(user.getName());
+            }
+        }
+    }
+}
